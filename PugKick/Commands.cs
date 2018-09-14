0c82102665fc3d2627aa70ec9619345eb864fd42
Von Trinks <err0r142@gmail.com>
2018-09-13 17:30:25 -0700

    inital commit

---
+++ PugKick/Commands.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.Rest;
+using Discord.WebSocket;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace PugKick
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+
+        [Command("roles")]
+        public async Task Roles()
+        {
+            List<ulong> roles = new List<ulong>();
+            List<ulong> memberIDs = new List<ulong>();
+
+
+            var rolesFromGuild = Context.Guild.Roles.ToList();
+            var gUsers = Context.Guild.Users.ToList();
+            for (int i = 0; i < gUsers.Count; i++)
+            {
+                await ReplyAsync($"{gUsers[i]}");
+            }
+        }
+
+            //for (int i = 0; i < Context.Guild.Roles.Count; i++)
+            //{
+            //    roles.Add(rolesFromGuild[i]);
+            //    memberIDs.Add(gUsers[i]);
+               
+            //}        }
+
+        [Command("userinfo"), Summary("Returns info about the current user, or the user parameter, if one passed.")]
+        [Alias("user", "whois")]
+        public async Task UserInfo([Summary("The (optional) user to get info for")] IUser user = null)
+        {
+            var userInfo = user ?? Context.Client.CurrentUser;
+            await ReplyAsync($"{userInfo.Username}#{userInfo.Discriminator}");
+            await ReplyAsync($"{userInfo.Id}");
+
+            for (int i = 0; i < Context.Guild.Users.Count; i++)
+            {
+                
+            }
+        }
+    }
+
+
+    
+
+
+    
+}
--- PugKick/Program.cs
+++ PugKick/Program.cs
@@ -1,12 +1,74 @@
 ﻿using System;
+using System.Reflection;
+using System.IO;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.Collections.Generic;
 
 namespace PugKick
 {
     class Program
     {
+            private DiscordSocketClient Client;
+        private CommandService Commands;
         static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+        private async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+            Client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Debug
+            });
+
+            Commands = new CommandService(new CommandServiceConfig
+            {
+                CaseSensitiveCommands = true,
+                DefaultRunMode = RunMode.Async,
+                LogLevel = LogSeverity.Debug
+
+            });
+
+            Client.MessageReceived += Client_MessageRecieved;
+            await Commands.AddModulesAsync(Assembly.GetEntryAssembly());
+            Client.Ready += Client_Ready;
+            Client.Log += Client_Log;
+
+            string Token = System.Environment.GetEnvironmentVariable("PUG_TOKEN");
+            await Client.LoginAsync(TokenType.Bot, Token);
+            await Client.StartAsync();
+            await Task.Delay(-1);
+        }
+
+        private async Task Client_Log(LogMessage Message)
+        {
+            Console.WriteLine($"{DateTime.Now} at {Message.Source}] {Message.Message}");
+        }
+
+        private async Task Client_MessageRecieved(SocketMessage MessageParam)
+        {
+            var Message = MessageParam as SocketUserMessage;
+            var Context = new SocketCommandContext(Client, Message);
+
+            if (Context.Message == null || Context.Message.Content == "") return;
+            if (Context.User.IsBot) return;
+
+            int ArgPos = 0;
+            if (!(Message.HasCharPrefix('!', ref ArgPos) || Message.HasMentionPrefix(Client.CurrentUser, ref ArgPos))) return;
+
+            var Result = await Commands.ExecuteAsync(Context, ArgPos);
+            if (!Result.IsSuccess)
+                Console.WriteLine($"{DateTime.Now} at Commands] Something went wrong with executing command. Text: {Context.Message.Content} | Error: {Result.ErrorReason}");
+
+
+
+
+        }
+        private async Task Client_Ready()
+        {
+            await Client.SetGameAsync("Rock Paper Scissors Bot!", "https://discordapp.com/developers", StreamType.NotStreaming);
         }
     }
+    
 }
